74b4e6ee60eef88b1e7ca0c404a2076974f16a3c
Binqi Shen <binqishen2021@u.northwestern.edu>
2021-05-20 00:14:55 -0500

    Modify import statements in flask-app-related files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import traceback
 import logging.config
+
 from flask import Flask
 from flask import render_template, request, redirect, url_for
+from src.add_application import Application, ApplicationManager
 
 # Initialize the Flask application
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
@@ -16,8 +18,9 @@ logger = logging.getLogger(app.config["APP_NAME"])
 logger.debug('Web app log')
 
 # Initialize the database session
-from src.add_application import Application, ApplicationManager
 application_manager = ApplicationManager(app)
+GENDERS = ['Male', 'Female']
+
 
 @app.route('/')
 def index():
@@ -33,27 +36,46 @@ def index():
     try:
         tracks = application_manager.session.query(Application).limit(app.config["MAX_ROWS_SHOW"]).all()
         logger.debug("Index page accessed")
-        return render_template('index.html', tracks=tracks)
+        return render_template('index.html', tracks=tracks, genders=GENDERS)
     except:
         traceback.print_exc()
         logger.warning("Not able to display tracks, error page returned")
         return render_template('error.html')
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/add', methods=['POST', 'GET'])
 def add_entry():
     """View that process a POST with new song input
 
     :return: redirect to index page
     """
+    if request.method == 'GET':
+        return "Visit the homepage to add songs"
+    elif request.method == 'POST':
+        try:
+            application_manager.add_track(
+                artist=request.form['artist'],
+                album=request.form['album'],
+                title=request.form['title']
+            )
+            logger.info(
+                "New song added: %s by %s",
+                request.form['title'],
+                request.form['artist']
+            )
+            return redirect(url_for('index'))
+        except:
+            logger.warning("Not able to display tracks, error page returned")
+            return render_template('error.html')
 
-    try:
-        application_manager.add_track(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
-        logger.info("New song added: %s by %s", request.form['title'], request.form['artist'])
-        return redirect(url_for('index'))
-    except:
-        logger.warning("Not able to display tracks, error page returned")
-        return render_template('error.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    """Add an 'about' page
+
+    :return: redirect to the 'about' page
+    """
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
